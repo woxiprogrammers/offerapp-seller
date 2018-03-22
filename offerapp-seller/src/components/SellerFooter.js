9ea02e89c088878a961c2f940948846cbad0d250
mayur-woxi <mayur.woxi@gmail.com>
2018-03-22 10:45:18 +0530

    Grab offer popup UI completed

--- offerapp-seller/src/components/SellerFooter.js
+++ offerapp-seller/src/components/SellerFooter.js
@@ -4,21 +4,38 @@ import { Container,
   Text,  
   Footer,
   FooterTab, } from 'native-base';
-  import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import prompt from 'react-native-prompt-android';
+import { Alert } from 'react-native';
 
-
-export default class SelelrFooter extends Component {
+export default class SellerFooter extends Component {
   render() {
     return (
       
       <Container>
           <Footer style={{backgroundColor: '#C10F41'}}>
             <FooterTab>
-                <Button vertical>
+                <Button vertical  
+                onPress={() => {
+                  prompt(
+                    'Enter Grab Code',
+                    "Enter customer's Grab Code to complete the transaction",
+                    [
+                      {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                      {text: 'OK', onPress: grabCode => console.log('OK Pressed, Grab Code: ' + grabCode)},
+                    ],
+                    {
+                    }
+                  );
+                }}>
+                    
                     <FontAwesome name="handshake-o" size={22} color='white' />
                     <Text style={{color:'white'}}>Grab Offer</Text>
                 </Button>
-                <Button vertical color='white'>
+                <Button vertical 
+                  onPress={() => {
+                    Alert.alert('Work in Progress');
+                  }}>
                     <FontAwesome name="filter" size={22}  style={{color:'white'}}/>  
                     <Text style={{color:'white'}}>Filter</Text>
                 </Button>
