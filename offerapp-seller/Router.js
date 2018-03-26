4597c25d5bd76353ee2845a09710ed6243a28346
mayur-woxi <mayur.woxi@gmail.com>
2018-03-26 11:59:24 +0530

    I am Intrested page UI completed

--- offerapp-seller/Router.js
+++ offerapp-seller/Router.js
@@ -3,7 +3,7 @@ import { Scene, Router, Drawer, Stack, Overlay } from 'react-native-router-flux'
 import OfferListing from './src/screens/OfferListing';
 import CreateOffer from './src/screens/CreateOffer';
 import AddToGroup from './src/screens/AddToGroup'
-
+import IamIntrested from './src/screens/IamIntrested'
 class RouterComponent extends React.Component {
   render() {
     console.log('Router Running !!');
@@ -14,6 +14,7 @@ class RouterComponent extends React.Component {
                   <Scene hideNavBar key="OfferListingScreen" component={OfferListing} />
                   <Scene hideNavBar key="CreateOfferScreen" component={CreateOffer} />
                   <Scene hideNavBar key="AddToGroupScreen" component={AddToGroup} />
+                  <Scene hideNavBar key="IamIntrestedScreen" component={IamIntrested} />
                 </Stack>
          
       </Router>
---
+++ offerapp-seller/src/components/CustomerCard.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { Card, CardItem, Body, Left, Text, Right, Button } from 'native-base';
+import { View, StyleSheet } from 'react-native';
+import {
+    responsiveWidth,
+    responsiveFontSize,
+    responsiveHeight
+  } from 'react-native-responsive-dimensions';
+import prompt from 'react-native-prompt-android';
+
+export default class CustomerCard extends Component {
+    render(){
+        return(
+            <Card>
+                <CardItem>
+                    <Body>
+                        <View style={{
+                            flexDirection: 'row'
+                        }}
+                        >
+                        {/* Customer Details */}
+                            <Left>
+                                <Text style={styles.TextSize}>Name: {this.props.customerName} </Text>
+                                <Text style={styles.TextSize}>Mobile No.: {this.props.customerMobile} </Text>
+                                <Text style={styles.TextSize}>Email: {this.props.customerEmail} </Text>
+                            </Left>
+
+                            {/* Grab code Button */}
+                            <Right>
+                                <Button small
+                                    onPress= {() =>{
+                                        prompt(
+                                            'Enter Grab Code',
+                                            "Enter customer's Grab Code to complete the transaction",
+                                            [
+                                            {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                                            {text: 'OK', onPress: grabCode => console.log('OK Pressed, Grab Code: ' + grabCode)},
+                                            ],
+                                            {
+                                            }
+                                        );
+                                    }}
+                                >
+                                    <Text style={styles.TextSize}>Grab Code</Text>
+                                </Button>
+                            </Right>
+                        </View>
+                    </ Body>
+                </CardItem>
+            </Card>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    TextSize:{
+        fontSize: responsiveFontSize(2)
+    }
+})
